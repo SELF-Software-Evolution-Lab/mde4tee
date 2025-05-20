d47c603e34e9f64e33128513af2ff1b4ad37bc58
j.jauregui@uniandes.edu.co <j.jauregui@uniandes.edu.co>
2025-05-20 13:51:46 -0500

    Add code generator

---
+++ mainworkspace/02-codegenerator/org.eclipse.acceleo.sample/src/fuentes/IntegrationSolution_launcher.c
@@ -0,0 +1,21 @@
+
+/* -------------------------------------------------- */
+/*                NON-SECURE LAUNCHER                 */
+/* Generated from IntegrationSolution: IntegrationSolution */
+/* -------------------------------------------------- */
+
+#include <stdio.h>
+
+/* Integration Process */
+/* Integration Process: Secure Integration Process */
+
+int main() {
+    printf("Launching secure application...\n");
+    
+    // TODO: Initialize communication with secure app
+
+    return 0;
+}
+
+/* End of Launcher */
+
---
+++ mainworkspace/02-codegenerator/org.eclipse.acceleo.sample/src/fuentes/IntegrationSolution_secure_app.c
@@ -0,0 +1,26 @@
+
+/* -------------------------------------------------- */
+/*              SECURE APPLICATION                   */
+/* Generated from IntegrationSolution: IntegrationSolution */
+/* -------------------------------------------------- */
+
+#include <stdio.h>
+
+/* Memory Compartment: SecureProgram */
+
+typedef struct {
+    int id;
+	char name[50];
+} SecureProgram_t;
+
+/* Example variable for memory compartment */
+SecureProgram_t secureProgram = { 1, "SecureProgram" };
+
+
+/* Port: IN (ID: 1) */
+void handle_IN(void) {
+    // TODO: Implement Port IN
+}
+
+/* End of Secure App */
+
